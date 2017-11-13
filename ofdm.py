29fe60cc47b6ed9a047e2e4e69d803741aec4e77
mauricio romero <mau.altgr@hotmail.com>
2017-11-12 22:19:20 -0300

    channel estimation-BER

--- ofdm.py
+++ ofdm.py
@@ -16,48 +16,35 @@ FALTA:	- Estimacion de canal (pilotos).
 	- Proponer canal acústico.
 """
 
-def s2p(x): # serial a paralelo
+def s2p(x):
+	"""Conversor serial a paralelo."""
 	n = int(len(x)/2)
 	xr = np.reshape(x,(n,2))
 	return xr
 
 
-def mapping(x, mapa): # mapea la info a qpsk
+def mapping(x, mapa):
+	"""Mapa de bits a simbolos QPSK."""
 	return np.array([mapa[tuple(xi)] for xi in x])
 
 
-def win(nw, x): # x: simbolo + cp, nw:muestras de ventana
-	nx = x.size
-	n1 = np.arange(0, nw, 1) 
-	n3 = np.arange(nw + nx, 2*nw + nx, 1)
-	w1 = 0.5*(1 - np.cos(np.pi*n1/nw))
-	w2 = x
-	w3 = 0.5*(1 - np.cos(np.pi*(n3 - 2*nw - nx)/nw))
-	w = np.concatenate((w1, w2, w3))
-	return w
-
-
-def plot(x, y):
-	plt.plot(x, y)
-	plt.show()
-	return
-
-
 def fft(x):
+	"""Transformada rapida de Fourier."""
 	xf = np.fft.fft(x)
 	xmax = np.max(np.abs(xf))
 	y = np.fft.fftshift(20*np.log10(np.abs(xf)/xmax))
 	return y
 
-# agrega prefijo final y comienzo, x senal, n numero de muestras
+
 def prefix(x, n):
-	a1 = x[0:n]
-	a2 = x[-1*n:]
-	X = np.concatenate((a2, x, a1))
+	"""Prefijo de la señal OFDM."""
+	a1 = x[-1*n:]
+	X = np.concatenate((a1, x))
 	return X
 
 
 def raised(x, n, b): # b<0.7
+	"""Ventana raised cosine."""
 	ns = len(x) - 2*n
 	bns = int(np.rint(b*ns))
 	N1 = np.arange(0, bns, 1.)
@@ -70,28 +57,69 @@ def raised(x, n, b): # b<0.7
 
 
 def norma(y,x,h):
+	"""Norma de un vector."""
 	fun = y - np.dot(x, h)
 	return np.real(np.dot(fun, np.conj(fun)))
 
 
 def LMS(y, x, alpha=1e-3, tol=1e-2):
+	"""Estimador de minimos cuadrados."""
 	q = tol + 1	
 	hn1 = np.zeros(len(y))
 	while q>tol:
 		hn2 = hn1 + 2*alpha*np.dot((y - np.dot(x,hn1)).T, x)
 		q = np.abs(norma(y, x, hn1) - norma(y, x, hn2))
 		hn1 = hn2
-		print 'NO CONVERGE'
-		print hn1
+		#print 'NO CONVERGE'
+		#print hn1
 	return hn1
 
+
 def DEC(x,y):
+	"""Mecanismo de decision sobre el simbolo recibido en base a la distancia al simbolo."""
 	yy = []
 	for xi in x:
 		yy.append(np.abs(y-xi)**2)
 	yy = np.array(yy).T
 	args = np.argmin(yy, axis=1)
 	return x[args]
+
+
+def inter(h, p, f):
+	"""Interpola (lineal) la respuesta en frecuencia del canal"""
+	hnew = []
+	Nf = int(f.shape[0]*0.5)
+	Np = int(p.shape[0]*0.5)
+	f = np.concatenate((f[Nf:], f[:Nf]))
+	p = np.concatenate((p[Np:], p[:Np]))
+	h = np.concatenate((h[Np:], h[:Np]))
+	hnew = np.interp(f, p, h)
+	return np.fft.fftshift(f), np.fft.fftshift(hnew)
+
+
+def demapping(x, mapa):
+	"""Realiza demapping."""
+	X = []
+	for xi in x:
+		Xi = []
+		for xir, xii in zip(np.real(xi), np.imag(xi)):
+			Xi.append(mapa[xir, xii])
+		Xi = np.array(Xi)
+		X.append(Xi)
+	X = np.array(X)
+	return X
+
+def par2ser(x):
+	"""Conversor paralelo serial."""
+	X = []
+	for xi in x:
+		Xi = []
+		for xii in xi:
+			Xi.append(xii[0])
+			Xi.append(xii[1])
+		Xi = np.array(Xi)		
+		X.append(Xi)			
+	return np.array(X)
 
 
 # distancia entre portadoras
@@ -105,10 +133,15 @@ fs = 44100
 ts = 1./fs # periodo de muestreo
 
 # constelacion QPSK
-cons = {(0,0): 1+1j, 
-	(0,1): -1+1j,
-	(1,1): -1-1j,
-	(1,0): 1-1j}
+cons = {(0, 0):  1+1j, 
+	(0, 1): -1+1j,
+	(1, 1): -1-1j,
+	(1, 0):  1-1j}
+
+decons = {( 1,  1): np.array([0, 0]), 
+	  (-1,  1): np.array([0, 1]), 
+	  (-1, -1): np.array([1, 1]),
+	  ( 1, -1): np.array([1, 0])}
 
 # numero de carriers, minimo 16
 m = 64 
@@ -145,18 +178,21 @@ elif nombre=='rect':
 # crea vector temporal OFDM
 S = []
 # numero de simbolos OFDM a enviar
-k = 100
+k = 1
 # parametro raised cosine
 beta = 0.4
 # numero de muestras prefijos
 ene = 16
 # vector de simbolos enviados 
 SN = []
+#informacion serial
+DS = []
 
 # crea la senal OFDM desde la generaciond de los datos hasta windowing
 for i in range(k):
 	# senal a enviar, aleatoria
 	senal = np.random.binomial(1, 0.5, P*u)
+	DS.append(senal)
 	# serial a paralelo		
 	s_p = s2p(senal) 
 	# mapping
@@ -179,6 +215,7 @@ for i in range(k):
 	S = np.concatenate((S, s_ofdm_t_r))
 	SN.append(sn)
 
+DS = np.array(DS)
 SN = np.array(SN)
 #plt.plot(S, linewidth=0.5)
 #plt.show()
@@ -190,7 +227,9 @@ SN = np.array(SN)
 #s_i = np.real(S)
 #s_q = np.imag(S)
 
-tc = np.linspace(0, Tfft/64*9600, 9600)
+# periodo de todos los simbolos
+T = Tfft/64*len(S)
+tc = np.linspace(0, T, len(S))
 """
 cc = np.cos(2*np.pi*fc*tc)
 cs = np.sin(2*np.pi*fc*tc)
@@ -239,118 +278,133 @@ plt.clf()
 # Canal
 ############################
 
-# ruido AWGN 
-nt = np.random.normal(loc=0, scale=1e1, size=len(S))
-# potencia promedio del ruido
-noise_power = np.trapz(nt**2, tc)/1.5
 # potencia promedio de la senal
-signal_power = np.trapz(S**2, tc)/1.5
-# snr promedio
-snr = 10*np.log10(signal_power/noise_power)
-print snr, 'dB'
-# canal
-h = 1
-# rudio + canal
-r = S*h + nt
-"""
-# plot espectral ruido
-plt.clf()
-plt.plot(np.fft.fftshift(np.fft.fftfreq(len(nt), d=dt)), fft(nt), linewidth=0.5)
-plt.xlabel('Frecuencia Hz')
-plt.ylabel('Potencia dB')
-plt.title('Ruido')
-plt.show()
-
-# plot espectral senal OFDM
-plt.clf()
-plt.plot(np.fft.fftshift(np.fft.fftfreq(len(S), d=dt)), fft(S), linewidth=0.5)
-plt.xlabel('Frecuencia Hz')
-plt.ylabel('Potencia dB')
-plt.title(r'Senal')
-plt.show()
-
-# plot espectral senal OFDM + canal
-plt.clf()
-plt.plot(np.fft.fftshift(np.fft.fftfreq(len(r), d=dt)), fft(r), linewidth=0.5)
-plt.xlabel('Frecuencia Hz')
-plt.ylabel('Potencia dB')
-plt.title(r'Senal con ruido, snr '+str(np.around(snr, 0))+' dB')
-plt.show()
-"""
-#######################################################
-# Receptor
-#######################################################
-
-# guard interval removal
-
+signal_power = np.trapz(S**2, tc)/T
+# snr en dB
+vec_snr = np.arange(-10, 31, 1)
+BER = []
 # selecciona una muestra aleatoria sobre la cual comenzar el removal. 
-t = int(np.random.uniform()*16)
-#t = 15
-mu = np.arange(0, len(r), 1)
-
+t = int(np.random.uniform()*ene)
+#t = 16
+print 'FFT time synchronization error:', 16 - t, 'muestras'
 # pilotos y nulls
-pilotos = np.concatenate(([0, 32], pilotos, [33, 41, 49, 57]))
+pilotos = np.concatenate((pilotos, [33, 41, 49, 57]))
 pilotos = np.sort(pilotos)
+pilotos1 = np.array([7, 15, 23, 31, -31, -23, -15, -7])
+datos1 = np.delete(np.arange(0, 64, 1), [0, 32])
+freq = np.concatenate((np.arange(1, 32, 1), np.arange(-31, 0,1)))
+for snr in vec_snr:
+	# ruido AWGN
+	sigma = 10**(- snr * 0.1) * signal_power
+	nt = np.random.normal(loc=0, scale=np.sqrt(sigma), size=len(S))
+	# potencia promedio del ruido
+	noise_power = np.trapz(nt**2, tc)/1.5
+	# snr promedio
+	SNR = 10*np.log10(signal_power/noise_power)
+	#print snr, 'dB' 
+	#print SNR, 'dB'
+	# canal
+	h = 1
+	# rudio + canal
+	r = np.convolve(S, h, mode='same') + nt
+	"""
+	# plot espectral ruido
+	plt.clf()
+	plt.plot(np.fft.fftshift(np.fft.fftfreq(len(nt), d=dt)), fft(nt), linewidth=0.5)
+	plt.xlabel('Frecuencia Hz')
+	plt.ylabel('Potencia dB')
+	plt.title('Ruido')
+	plt.show()
 
-# recorre los 100 simbols ofdm
-block_fft = []
-block_fft_raw = []
-freq = np.concatenate((np.arange(0, 32, 1), np.arange(-32, 0,1)))
-sim = mapping([[0, 0], [0, 1], [1, 1], [1, 0]], cons)
-for i in range(k):
-	# selecciona un bloque de largo 64 partiendo en t
-	# arregla error de sincronizacion 
-	block = r[t+96*i:t+96*i+64]
-	# DFT
-	blockfft = np.fft.fft(block, n=len(block))
-	# guarda data en bruto	
-	block_fft_raw.append(DEC(sim, blockfft))
-	# LMS estimator
-	# simbolos enviados por nulls y pilots
-	X = np.diag(SN[i][pilotos])
-	Y = blockfft[pilotos]
-	H = LMS(Y, X)
-	H=H/np.sqrt(np.real(np.dot(H, np.conj(H))))
-	# decide simbolos
-	blockfft[pilotos] = blockfft[pilotos]*np.conj(H)
+	# plot espectral senal OFDM
+	plt.clf()
+	plt.plot(np.fft.fftshift(np.fft.fftfreq(len(S), d=dt)), fft(S), linewidth=0.5)
+	plt.xlabel('Frecuencia Hz')
+	plt.ylabel('Potencia dB')
+	plt.title(r'Senal')
+	plt.show()
+
+	# plot espectral senal OFDM + canal
+	plt.clf()
+	plt.plot(np.fft.fftshift(np.fft.fftfreq(len(r), d=dt)), fft(r), linewidth=0.5)
+	plt.xlabel('Frecuencia Hz')
+	plt.ylabel('Potencia dB')
+	plt.title(r'Senal con ruido, snr '+str(np.around(snr, 0))+' dB')
+	plt.show()
+	"""
+	#######################################################
+	# Receptor
+	#######################################################
+
+	# guard interval removal	
+	mu = np.arange(0, len(r), 1)
+
+	# recorre los 100 simbols ofdm
+	block_fft = []
+	block_fft_raw = []
 	
-	block_fft.append(DEC(sim, blockfft))#*np.exp(1j*2*np.pi*freq*(16 - t)/64))
+	sim = mapping([[0, 0], [0, 1], [1, 1], [1, 0]], cons)
+	plt.clf()
+	for i in range(k):
+		# selecciona un bloque de largo m=64 partiendo en t
+		# arregla error de sincronizacion 
+		block = r[t+(m + ene)*i:t+(m + ene)*i+m]
+		# DFT
+		blockfft = np.fft.fft(block, n=m)
+		# guarda data en bruto	
+		block_fft_raw.append(blockfft)
+		# channel estimation
+		# LMS estimator
+		# simbolos enviados por nulls y pilots
+		#X = np.diag(SN[i][pilotos])
+		#Y = blockfft[pilotos]
+		#H = LMS(Y, X)
+		#H = blockfft[pilotos]/SN[i, pilotos]		
+		# interpola el canal
+		#fi, Hi = inter(H, pilotos1, freq)
+		#H=H/np.sqrt(np.real(np.dot(H, np.conj(H))))
+		# decide simbolos
+		# arg de los no nulls
+		#arg = np.delete(np.arange(0, 64, 1), [0, 32])
+		#blockfft[arg] = blockfft[arg]/Hi	
+		block_fft.append(DEC(sim, blockfft))
+		
+			
+	block_fft = np.array(block_fft)
+	block_fft_raw = np.array(block_fft_raw)
 	
-block_fft = np.array(block_fft)
-block_fft_raw = np.array(block_fft_raw)
-
-# determina simbolos erroneos
-
+	# grafica los simbolos en el espacio complejo
+	plt.clf()
+	plt.scatter(np.real(block_fft_raw)[:,datos1], np.imag(block_fft_raw)[:,datos1], marker='+', linewidth=0.5)
+	plt.scatter([1, 1, -1, -1], [1, -1, 1, -1], color='black', marker='x')
+	plt.xlabel('real')
+	plt.ylabel('imaginaria')
+	plt.title('Mapa de simbolos qpsk')
+	plt.savefig('/home/mauricio/Documents/Uni/OFDM/Images/symbols_'+str(snr))
+	
+	# determina bits erroneos
+	datos_tot = np.delete(np.arange(0, 64, 1), pilotos)
+	# selecciona carriers de datos
+	rec = block_fft[:, datos]
+	# demap
+	rec_demap = demapping(rec, decons)
+	# conversion paralela-serial
+	DSR = par2ser(rec_demap)
+	# bit erroneos
+	ber_per_symbol = np.sum(DSR!=DS, axis=1)/P/u
+	ber_avg = np.mean(ber_per_symbol)
+	BER.append(ber_avg)
+BER = np.array(BER)
 plt.clf()
-plt.plot(np.sum(np.equal(block_fft, SN), axis=1)/64*100, label='canal estimado')
-plt.plot(np.sum(np.equal(block_fft_raw, SN), axis=1)/64*100, label='sin estimacion')
-plt.title('Error OFDM SNR='+str(int(np.rint(snr)))+' dB')
-plt.xlabel('simbolo OFDM')
-plt.ylabel('error $\%$')
-plt.legend()
+plt.semilogy(vec_snr, BER, marker='x')
+plt.xlabel('SNR dB')
+plt.ylabel('BER')
+plt.grid()
 plt.show()
+
+
+
 """
-
-
-
-
-
-plt.clf()
-plt.plot(np.real(H))
-plt.plot(np.imag(H))
-plt.show()
-
-# grafica los simbolos en el espacio complejo
-#ui = 31
-plt.clf()
-for i in block_fft:
-	plt.scatter(np.real(i), np.imag(i), marker='.')
-plt.scatter([1, 1, -1, -1], [1, -1, 1, -1], color='black', marker='x')
-plt.xlabel('real')
-plt.ylabel('imaginaria')
-plt.title('Mapa de simbolos qpsk')
-plt.show()
-
 # grafica los pilotos en el espacio complejo + ceros
 plt.clf()
 for i in block_fft:
@@ -365,7 +419,11 @@ plt.xlabel('real')
 plt.ylabel('imaginaria')
 plt.title('Mapa de simbolos qpsk pilotos')
 plt.show()
+"""
+"""
+plot impulso
 
+plt.stem(datos*1e-1, np.ones(len(datos)), 'b', markerfmt='bo', label='datos', basefmt=" ")
 """
 
 
